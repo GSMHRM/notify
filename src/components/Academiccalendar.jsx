7f1ac5ecba241d22af1185c21462eb7e1fbdc470
한준 <s22073@gsm.hs.kr>
2023-03-02 16:49:15 +0900

    fix catch academiccalendar

--- src/components/Academiccalendar.jsx
+++ src/components/Academiccalendar.jsx
@@ -21,11 +21,15 @@ const URL = `	https://open.neis.go.kr/hub/SchoolSchedule?KEY=${API_KEY}&Type=jso
 
 const Academiccalendar = () => {
   const [data, setData] = useState([]);
+  const [message, setmessage] = useState(false);
 
   useEffect(() => {
     axios.get(URL).then((res) => {
       console.log(res.data.SchoolSchedule[1]);
       setData(res.data.SchoolSchedule[1].row);
+      setmessage(true);
+    }).catch((err)=>{
+      setmessage(false);
     });
   }, []);
 
@@ -45,11 +49,11 @@ const Academiccalendar = () => {
         <div className={styles.main}>
           <p className={styles.now__dust}>이번달 학사 일정은</p>
           <hr />
-          {data.map((eventnm, index) => (
+          {message?data.map((eventnm, index) => (
             <p key={index} style={{ fontSize: "0.6rem", float:"left",margin:"1.6%", marginLeft:"3%" }}>
               {eventnm.EVENT_NM} ({eventnm.AA_YMD})
             </p>
-          ))}
+          )): <p style={{fontSize:"1rem"}}>일정이 없습니다.</p>}
         </div>
         <Link to={`/testschedule`} className={styles.prebutton}>
           {">"}
